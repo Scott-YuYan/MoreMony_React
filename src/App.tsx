0bd04c4483b5b8d1219a969a770941d472051c5d
Scoot_yuyan <mryuyan97@gmail.com>
2021-02-20 11:18:48 +0800

    实现基本样式布局

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,58 @@ import {
     Redirect,
     useLocation,
 } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow: auto;
+`
+
+const Nav = styled.div`
+  border: 1px solid black;
+  text-align: center;
+  padding: 10px;
+  background-color: blue;
+
+  > ul {
+    display: flex;
+
+    > li {
+      width: 33.33333%;
+    }
+  }
+`
 
 function App() {
     return (
         <Router>
-            <div>
-                <nav>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/label">
+                            <Label/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistic">
+                            <Statistic/>
+                        </Route>
+                        <Redirect exact from="/" to="statistic"/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+                <Nav>
                     <ul>
                         <li>
                             <Link to="/label">标签</Link>
@@ -25,24 +71,8 @@ function App() {
                             <Link to="/statistic">统计</Link>
                         </li>
                     </ul>
-                </nav>
-
-                <Switch>
-                    <Route path="/label">
-                        <Label/>
-                    </Route>
-                    <Route path="/money">
-                        <Money/>
-                    </Route>
-                    <Route path="/statistic">
-                        <Statistic/>
-                    </Route>
-                    <Redirect exact from="/" to="statistic"/>
-                    <Route path="*">
-                        <NoMatch/>
-                    </Route>
-                </Switch>
-            </div>
+                </Nav>
+            </Wrapper>
         </Router>
     );
 }
