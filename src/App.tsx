8bdf107719cd210553179120e11dd88eb32e90c2
Scoot_yuyan <mryuyan97@gmail.com>
2021-02-20 10:22:10 +0800

    实现基本的导航页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,74 @@
 import React from 'react';
-import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useLocation,
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/label">标签</Link>
+                        </li>
+                        <li>
+                            <Link to="/money">记一笔</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistic">统计</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/label">
+                        <Label/>
+                    </Route>
+                    <Route path="/money">
+                        <Money/>
+                    </Route>
+                    <Route path="/statistic">
+                        <Statistic/>
+                    </Route>
+                    <Redirect exact from="/" to="statistic"/>
+                    <Route path="*">
+                        <NoMatch/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+function Label() {
+    return <h2>标签页</h2>;
+}
+
+function Money() {
+    return <h2>记一笔</h2>;
+}
+
+function Statistic() {
+    return <h2>统计</h2>;
+}
+
+function NoMatch() {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h3>
+                No match for <code>{location.pathname}</code>
+            </h3>
+        </div>
+    );
 }
 
 export default App;
