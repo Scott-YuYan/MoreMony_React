695b9ac5408a4fac0537abbc0a363c9dcc85ad7f
Scoot_yuyan <mryuyan97@gmail.com>
2021-02-22 15:42:12 +0800

    增加标签被选中后的样式

--- src/views/money/TagsSection.tsx
+++ src/views/money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const TagsSection = styled.section`
+const TagSectionWrapper = styled.section`
   background-color: #FFF;
   padding: 12px 16px;
   display: flex;
@@ -8,9 +9,9 @@ const TagsSection = styled.section`
   align-items: flex-start;
   justify-content: flex-end;
   flex-grow: 1;
-  
+
   > ol {
-    
+
     margin: 0px -12px;
 
     > li {
@@ -20,6 +21,9 @@ const TagsSection = styled.section`
       padding: 3px 18px;
       margin: 8px 12px;
       font-size: 14px;
+      &.selected {
+        background: #9b9b9b;
+      }
     }
   }
 
@@ -31,6 +35,42 @@ const TagsSection = styled.section`
     color: #666;
     margin-top: 12px;
   }
-
 `
+
+const TagsSection: React.FunctionComponent = () => {
+    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    const addTags = () => {
+        const newTagName = window.prompt("请选择需要添加的标签");
+        if (newTagName !== null) {
+            setTags([...tags, newTagName])
+        }
+    };
+    const onToggleTag = (tag: string) => {
+        if (selectedTags.indexOf(tag) >= 0) {
+            //如果tag被选中，则取消该选中，需要将其他选中被选中的标签加到选中的数组中
+            setSelectedTags(selectedTags.filter(t => (t !== tag)));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    };
+
+    const selectedStyle = (tag:string)=>{
+        return selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
+    }
+    return (
+        <TagSectionWrapper>
+            <ol>{tags.map((tag) =>
+                <li key={tag} onClick={() => {
+                    onToggleTag(tag)
+                }} className={selectedStyle(tag)}>{tag}</li>
+            )
+            }
+            </ol>
+            <button onClick={addTags}>增加标签</button>
+        </TagSectionWrapper>
+    );
+};
+
+
 export default TagsSection;
