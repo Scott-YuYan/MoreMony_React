34481a7cd37020d418830d86c26d8a7fc74d603c
Scoot_yuyan <mryuyan97@gmail.com>
2021-02-23 10:05:08 +0800

    合并四个onchange

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,7 +5,6 @@ import TagsSection from "./money/TagsSection";
 import NotesSection from "./money/NotesSection";
 import CategorySection from "./money/CategorySection";
 import NumberPadSection from "./money/NumberPadSection";
-import {type} from "os";
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -20,37 +19,27 @@ function Money() {
         category: '-' as Category,
         amount: 0
     });
+    const onchange = (obj: Partial<typeof selected>) => {
+        setSelected({
+            ...selected, ...obj
+        })
+    }
     return (
         <MyLayout>
             {selected.tags.join(',')}
             {selected.note}
             {selected.category}
             {selected.amount}
-            <TagsSection value={selected.tags} onchange={
-                (tags) => setSelected({
-                    ...selected, tags: tags
-                })
-            }/>
-            <NotesSection value={selected.note} onchange={
-                (note) => {
-                    setSelected({
-                        ...selected, note: note
-                    })
-                }
-            }/>
+            <TagsSection value={selected.tags} onchange={(tags) =>
+                onchange({tags: tags})}/>
+            <NotesSection value={selected.note} onchange={(note) =>
+                onchange({note: note})}/>
             <CategorySection value={selected.category} onchange={(category) => {
-                setSelected({
-                        ...selected, category: category
-                    }
-                )
+                onchange({category: category})
+            }}/>
+            <NumberPadSection value={selected.amount} onchange={(amount) => {
+                onchange({amount: amount})
             }}
-            />
-            <NumberPadSection value={selected.amount}
-                              onchange={(amount: number) => {
-                                  setSelected({
-                                      ...selected, amount: amount
-                                  })
-                              }}
                               onOk={() => {
                               }}
             />
