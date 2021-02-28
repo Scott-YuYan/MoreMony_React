d2028fcbf631d3f7ed07398944e3f7a5386c178e
Scoot_yuyan <mryuyan97@gmail.com>
2021-02-28 10:14:56 +0800

    fix deploy bug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,10 @@
 
 yarn build &&
 cd build &&
+git init &&
 git add . &&
 git commit -m "update build" &&
-git init &&
 git remote add origin git@github.com:Scott-YuYan/MoreMoney-React-Website.git &&
 git push -u origin master -f
 cd -
+echo "deploy done"
